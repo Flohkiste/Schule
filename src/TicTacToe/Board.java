0cc7fc3b3089adf83e575c73f3af368b3cca40cd
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2023-03-30 18:07:09 +0200

    Added CheckWin function

--- src/TicTacToe/Board.java
+++ src/TicTacToe/Board.java
@@ -16,4 +16,19 @@ public class Board {
 
         }
     }
+
+    //function to check if somebody won the Game
+    public boolean checkWin(){
+        if((fields[0].equals("X") & fields[1].equals("X") & fields[2].equals("X")) | (fields[0].equals("X") & fields[3].equals("X") & fields[6].equals("X")) | (fields[0].equals("X") & fields[4].equals("X") & fields[8].equals("X")) | (fields[1].equals("X") & fields[4].equals("X") & fields[7].equals("X")) | (fields[2].equals("X") & fields[5].equals("X") & fields[8].equals("X")) | (fields[3].equals("X") & fields[4].equals("X") & fields[5].equals("X")) | (fields[6].equals("X") & fields[7].equals("X") & fields[8].equals("X")) | (fields[2].equals("X") & fields[4].equals("X") & fields[6].equals("X"))){
+            System.out.println("X hat gewonnen!!!");
+            return true;
+        }else if((fields[0].equals("O") & fields[1].equals("O") & fields[2].equals("O")) | (fields[0].equals("O") & fields[3].equals("O") & fields[6].equals("O")) | (fields[0].equals("O") & fields[4].equals("O") & fields[8].equals("O")) | (fields[1].equals("O") & fields[4].equals("O") & fields[7].equals("O")) | (fields[2].equals("O") & fields[5].equals("O") & fields[8].equals("O")) | (fields[3].equals("O") & fields[4].equals("O") & fields[5].equals("O")) | (fields[6].equals("O") & fields[7].equals("O") & fields[8].equals("O")) | (fields[2].equals("O") & fields[4].equals("O") & fields[6].equals("O"))){
+            System.out.println("Y hat gewonnen!!!");
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
 }
