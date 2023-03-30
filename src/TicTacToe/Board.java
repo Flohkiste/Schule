57203f35d4e0c46932b9934635a0901ff7871777
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2023-03-30 21:58:26 +0200

    worked on gameloop but not working at the moment

--- src/TicTacToe/Board.java
+++ src/TicTacToe/Board.java
@@ -1,12 +1,57 @@
 package TicTacToe;
 
+import java.util.Scanner;
+
 public class Board {
+
+    boolean currentPlayer = false;
+    boolean gameRunning = true;
+    int playerMove;
+    Scanner scanner = new Scanner(System.in);
+
     String[] fields = { "1", "2", "3",
                         "4", "5", "6",
                         "7", "8", "9" };
 
-    public Board(){
+    public void playerInput(){
+        while (gameRunning){
+            playerMove = scanner.nextInt();
+            if(playerMove < 1 | playerMove > 9){
+                System.out.println("Feld gibt es nicht, bitte Versuche es erneut mit einer Zahl zwischen 1 und 9!");
+            } else {
+                break;
+            }
+        }
+
+        checkField();
+
+    }
+
+    public void checkField(){
+        if(fields[playerMove-1].equals("X") | fields[playerMove-1].equals("O")){
+            System.out.println("Feld schon belegt bitte wähl ein freies!");
+            playerInput();
+        }
+    }
+
+
+    public void gameLoop(){
         drawBoard();
+
+        while (gameRunning){
+            playerInput();
+            makeTurn(playerMove-1);
+            checkWin();
+            drawBoard();
+            if (currentPlayer){
+                currentPlayer = false;
+            } else {
+                currentPlayer = true;
+            }
+
+
+        }
+
     }
 
 
@@ -21,10 +66,21 @@ public class Board {
             }
 
         }
+
+        if (currentPlayer){
+            System.out.println("O to make a move");
+        } else {
+            System.out.println("X to make a move");
+        }
+
     }
 
-    public void makeTurn(String player, int field){
-        fields[field-1] = player;
+    public void makeTurn(int field){
+        if (currentPlayer){
+            fields[field-1] = "X";
+        } else {
+            fields[field-1] = "O";
+        }
         drawBoard();
     }
 
@@ -33,9 +89,11 @@ public class Board {
     public boolean checkWin(){
         if((fields[0].equals("X") & fields[1].equals("X") & fields[2].equals("X")) | (fields[0].equals("X") & fields[3].equals("X") & fields[6].equals("X")) | (fields[0].equals("X") & fields[4].equals("X") & fields[8].equals("X")) | (fields[1].equals("X") & fields[4].equals("X") & fields[7].equals("X")) | (fields[2].equals("X") & fields[5].equals("X") & fields[8].equals("X")) | (fields[3].equals("X") & fields[4].equals("X") & fields[5].equals("X")) | (fields[6].equals("X") & fields[7].equals("X") & fields[8].equals("X")) | (fields[2].equals("X") & fields[4].equals("X") & fields[6].equals("X"))){
             System.out.println("X hat gewonnen!!!");
+            gameRunning = false;
             return true;
         }else if((fields[0].equals("O") & fields[1].equals("O") & fields[2].equals("O")) | (fields[0].equals("O") & fields[3].equals("O") & fields[6].equals("O")) | (fields[0].equals("O") & fields[4].equals("O") & fields[8].equals("O")) | (fields[1].equals("O") & fields[4].equals("O") & fields[7].equals("O")) | (fields[2].equals("O") & fields[5].equals("O") & fields[8].equals("O")) | (fields[3].equals("O") & fields[4].equals("O") & fields[5].equals("O")) | (fields[6].equals("O") & fields[7].equals("O") & fields[8].equals("O")) | (fields[2].equals("O") & fields[4].equals("O") & fields[6].equals("O"))){
             System.out.println("Y hat gewonnen!!!");
+            gameRunning = true;
             return true;
         }else {
             return false;
