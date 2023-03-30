84c269159b0f1943d1fa1b2ee0cb82d0f60d18f3
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2023-03-30 17:14:57 +0200

    updated Board.java

--- src/TicTacToe/Board.java
+++ src/TicTacToe/Board.java
@@ -1,4 +1,19 @@
 package TicTacToe;
 
 public class Board {
+    String[] fields = { "1", "2", "3",
+                        "4", "5", "6",
+                        "7", "8", "9" };
+
+    public void draw(){
+        for(int i = 0; i < 9; i++){
+            System.out.print(fields[i]);
+            if(i == 2 | i == 5 | i == 8){
+                System.out.println();
+            }else {
+                System.out.print("|");
+            }
+
+        }
+    }
 }
