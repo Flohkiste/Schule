b0667d35e00b6870ebcdd8fa188180399c678932
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2023-04-18 15:44:24 +0200

    Renamed Class

--- src/Arbeitsblatt_14/Zusatzaufgabe.java
+++ src/Arbeitsblatt_14/Zusatzaufgabe.java
@@ -2,7 +2,7 @@ package Arbeitsblatt_14;
 
 import java.util.Scanner;
 
-public class Zusatuaufgabe {
+public class Zusatzaufgabe {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         boolean isPalindrom = false;
