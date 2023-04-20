73b9635fd3b4807ba928d873290661cf9857b013
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2023-04-20 12:39:20 +0000

    Updated Aufgabe_3 from Arbeitsblatt_14

--- src/Arbeitsblatt_14/Aufgabe_3.java
+++ src/Arbeitsblatt_14/Aufgabe_3.java
@@ -5,65 +5,40 @@ import java.util.Scanner;
 public class Aufgabe_3 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        String SpecialCharacters = "!@#$%&*()'+,-./:;<=>?[]^_`{|}";
+        String upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String LowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "1234567890";
 
-        System.out.print("Bitte geben Sie ihre Passwort ein: ");
+        System.out.print("Bitte geben Sie ihr Passwort ein: ");
         String password = scanner.nextLine();
+        scanner.close();
 
-        if(lenght(password) & specialCharacter(password) & upperCaseCharacters(password) & lowerCaseCharacters(password)){
+        if(lenght(password) & checkfor(password, SpecialCharacters) & checkfor(password, upperCaseCharacters) & checkfor(password, LowerCaseCharacters) & checkfor(password, numbers)){
             System.out.println("Ihr Passwort ist sicher!");
         }else {
             System.out.println("Ihr Passwort ist unsicher Sie sollten sich ein neues ausdenken");
         }
 
     }
-
-    private static boolean lenght(String str){
-        if(str.length() >= 10){
-            return true;
-        }else {
-            return false;
-        }
-    }
-
-    private static boolean specialCharacter(String str){
-        String specialCharactersString = "!@#$%&*()'+,-./:;<=>?[]^_`{|}";
-        for (int i=0; i < str.length() ; i++) {
-            char ch = str.charAt(i);
-            if(specialCharactersString.contains(Character.toString(ch))) {
+   
+    private static boolean checkfor(String stringToBeChecked, String charactersToCheckfor) {
+        for (int i=0; i < stringToBeChecked.length() ; i++) {
+            char ch = stringToBeChecked.charAt(i);
+            if(charactersToCheckfor.contains(Character.toString(ch))) {
                 return true;
             }
-            else if(i == str.length()-1)
+            else if(i == stringToBeChecked.length()-1)
                 return false;
         }
         return false;
     }
 
-    private static boolean upperCaseCharacters(String str){
-        String specialCharactersString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        for (int i=0; i < str.length() ; i++) {
-            char ch = str.charAt(i);
-            if(specialCharactersString.contains(Character.toString(ch))) {
-                return true;
-            }
-            else if(i == str.length()-1)
-                return false;
-        }
-        return false;
+    private static boolean lenght(String stringToBeChecked) {
+    if(stringToBeChecked.length() >= 10) {
+    return true;
+    } else {
+    return false;
     }
-
-    private static boolean lowerCaseCharacters(String str){
-        String specialCharactersString = "abcdefghijklmnopqrstuvwxyz";
-        for (int i=0; i < str.length() ; i++) {
-            char ch = str.charAt(i);
-            if(specialCharactersString.contains(Character.toString(ch))) {
-                return true;
-            }
-            else if(i == str.length()-1)
-                return false;
-        }
-        return false;
     }
-
-
-
 }
