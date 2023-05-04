d39d1add8e2ee93f1778d65113ed2decf7304a33
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2023-05-04 12:32:56 +0000

    Added Arbeitsblatt_17 / Aufgabe_4

--- src/Arbeitsblatt_17/Aufgabe_4.java
+++ src/Arbeitsblatt_17/Aufgabe_4.java
@@ -1,7 +1,53 @@
 package Arbeitsblatt_17;
 
-public class Aufgabe_4 {
-    public static void main(String[] args){
+import java.util.Scanner;
 
-    }
+public class Aufgabe_4 {
+
+	public static void main(String[] args) {
+		boolean loopVariable = true;
+		Scanner scanner = new Scanner(System.in);
+		
+		//Eingabe
+		System.out.print("Anzahl der Schüler: ");
+		int numberOfStudents = scanner.nextInt();
+		System.out.println("");
+		double allGrades[][] = new double[numberOfStudents][];
+		
+		for(int x = 0; x < numberOfStudents; x++) {
+			System.out.print(x+1 + ". Schueler, wie viel Noten: ");
+			int numberOfGrades = scanner.nextInt();
+			double grades[] = new double[numberOfGrades];
+			System.out.println("");
+			for(int y = 0; y < numberOfGrades; y++) {
+				System.out.print(y+1 + ". Note: ");
+				grades[y] = scanner.nextDouble();
+			}
+			
+			allGrades[x] = grades;
+			
+			System.out.println("");
+		}
+		
+		
+		double gradeAvarage;
+		//Durchschnittsberechnung + Ausgabe
+		for(int x = 0; x < allGrades.length; x++) {
+			gradeAvarage = 0;
+			for(int y = 0; y < allGrades[x].length; y++) {
+				gradeAvarage += allGrades[x][y];
+			}
+			
+			gradeAvarage = gradeAvarage / allGrades[x].length;
+			
+			
+			System.out.println(x+1 + ". Schueler Durschnittsnote: " + gradeAvarage);
+			
+			
+		}
+		
+		
+		
+	}
+
 }
