95810b3b598eb78b8f25a3da15ee84d4523a8b49
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2023-09-25 11:33:33 +0000

    Aufgabe_3fertig

--- src/Klasse_12/Arbeitsblatt_1/Aufgabe_3.java
+++ src/Klasse_12/Arbeitsblatt_1/Aufgabe_3.java
@@ -1,7 +1,9 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Aufgabe_3 {
     public static Scanner sc;
+    public static Random random = new Random();
     public static void main(String[] args){
         sc = new Scanner(System.in);
         System.out.print("Gib deinen Namen ein: ");
@@ -9,6 +11,9 @@ public class Aufgabe_3 {
         
         hallo(name);
         potenz();
+        zufallszahlen(10);
+        teiler(10);
+        mittelwert();
     }
 
     public static void hallo(String name){
@@ -25,11 +30,27 @@ public class Aufgabe_3 {
 
     public static void zufallszahlen(int anzahl){
         for (int i = 0; i < anzahl; i++){
-            System.out.println(Math.random());
+            System.out.println(random.nextInt());
         }
     }
 
-    public static void 
-
-
+    public static void teiler(int zahl){
+        for (int i = 1; i <= zahl; i++){
+            if (zahl % i == 0){
+                System.out.println(i);
+            }
+        }
+    }
+ 
+    //Schreibe eine Methode welche den Mittelwert einer Menge von Zahlen berechnet und ausgibt.
+    public static void mittelwert(){
+        System.out.print("Gib die Anzahl der Zahlen ein: ");
+        int anzahl = sc.nextInt();
+        int summe = 0;
+        for (int i = 0; i < anzahl; i++){
+            System.out.print("Gib eine Zahl ein: ");
+            summe += sc.nextInt();
+        }
+        System.out.println("Der Mittelwert lautet: " + (summe / anzahl));
+    }
 }
