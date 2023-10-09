b3702613065a8e3f709031a01e247202ccce741b
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2023-10-09 13:19:51 +0200

    Update Aufgabe_2.java

--- src/Klasse_12/Software/Arbeitsblatt_2_2/Aufgabe_2.java
+++ src/Klasse_12/Software/Arbeitsblatt_2_2/Aufgabe_2.java
@@ -1,43 +1,50 @@
 package Klasse_12.Software.Arbeitsblatt_2_2;
 
 public class Aufgabe_2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(getBetrag(-10));
         System.out.println(getAnzahlZiffern(1241234));
         System.out.println(getZiffernWert(1234, 2));
         System.out.println(ersetzeZiffer(1234, 2, 5));
     }
 
-    public static int getBetrag(int pZahl){
-        if(pZahl < 0){
+    public static int getBetrag(int pZahl) {
+        if (pZahl < 0) {
             pZahl = pZahl * -1;
-        } 
+        }
 
         return pZahl;
     }
 
-    public static int getAnzahlZiffern(int pZahl){
+    public static int getAnzahlZiffern(int pZahl) {
         int x = 1;
 
-        while(true){
-            if((pZahl > (Math.pow(10, x)))){
+        while (true) {
+            if ((pZahl > (Math.pow(10, x)))) {
                 x++;
-            }else{
+            } else {
                 return x;
             }
         }
     }
 
-    public static int getZiffernWert(int pZahl, int pStelle){
+    public static int getZiffernWert(int pZahl, int pStelle) {
         String pZahlString = String.valueOf(pZahl);
         pZahl = Character.getNumericValue(pZahlString.charAt(pZahlString.length() - pStelle - 1));
         return pZahl;
     }
 
-    public static int ersetzeZiffer(int pZahl, int pStelle, int pWert){
+    public static int ersetzeZiffer(int pZahl, int pStelle, int pWert) {
         StringBuilder pZahlString = new StringBuilder(String.valueOf(pZahl));
         pStelle = pZahlString.length() - pStelle - 1;
         pZahlString.setCharAt(pStelle, Character.forDigit(pWert, 10));
         return Integer.parseInt(pZahlString.toString());
     }
+
+    public static int[] intToArray(int number) {
+        int numberLenght = getAnzahlZiffern(number);
+        int[] numberArray = new int[numberLenght];
+        return numberArray;
+    }
+
 }
