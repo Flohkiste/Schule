f21d713f4177f8cd786fa0b70ee5c74709661fb8
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2023-11-08 13:40:21 +0000

    Changed names of 4InARow

---
+++ src/VierGewinnt/Controller.java
@@ -0,0 +1,9 @@
+package VierGewinnt;
+
+public class Controller {
+    public static void main(String[] args) {
+        VierGewinnt vierGewinnt = new VierGewinnt();
+        vierGewinnt.startGame();
+    }
+}
+
