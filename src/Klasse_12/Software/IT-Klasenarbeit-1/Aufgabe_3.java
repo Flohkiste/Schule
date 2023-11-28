a0bf0be04e6334537264558d1d28d18be55b349f
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2023-11-28 07:31:15 +0000

    Added KA-Verbesserung

---
+++ src/Klasse_12/Software/IT-Klasenarbeit-1/Aufgabe_3.java
@@ -0,0 +1,11 @@
+
+public class Aufgabe_3{
+    public static void main(String[] args){
+        Figur figur1 = new Figur();
+        figur1.setBezeichnung("Martin");
+        figur1.setXPos(10);
+        figur1.setYPos(20);
+        System.out.println("Bezeichnung: " + figur1.getBezeichnung());
+    }
+    
+}
