d9ce2e43512e9dce8b513afec35e24799cbeb268
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2023-12-11 11:24:51 +0000

    neue Aufgabe

--- src/Klasse_12/Software/Arbeitsblatt_4_CT/Steuerung.java
+++ src/Klasse_12/Software/Arbeitsblatt_4_CT/Steuerung.java
@@ -6,11 +6,37 @@ public class Steuerung {
 
     static int index = 0;
     static Kontakt kontakte[] = new Kontakt[20];
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args){
 
-        for(Kontakt kontakt : kontakte){
-            kontakt = new Kontakt();
+        for(int i = 0; i < 20; i++){
+            kontakte[i] = new Kontakt();
+        }
+
+        while (true) {
+            System.out.println();
+            System.out.println("Dieses Programm realisiert eine Kontaktverwaltung.");
+            System.out.println("Zum beenden des Programms drücken sie (3).");
+            System.out.println("Möchten Sie Termine speichern (1) oder ausgeben (2)?");
+
+            int eingabe = scanner.nextInt();
+
+            if (eingabe == 1) {
+                if(index < 20){
+                    setKontakt();
+                }else{
+                    System.out.println("Liste Voll!!");
+                }
+            }else if (eingabe == 2) {
+                System.out.println("Welcher Datensatz soll ausgegeben werden? (1-20)");
+                eingabe = scanner.nextInt();
+                kontakte[eingabe - 1].ausgabe();
+            }else if(eingabe == 3){
+                break;
+            }else{
+                System.out.println("Falsche eingabe!!");
+            }
         }
 
 
@@ -19,7 +45,6 @@ public class Steuerung {
     }
 
     public static void setKontakt(){
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Geben Sie folgende Daten ein:");
 
@@ -40,6 +65,9 @@ public class Steuerung {
 
         System.out.print("PLZ: ");
         kontakte[index].setPlz(scanner.next());
+
+        System.out.print("Kommentar: ");
+        kontakte[index].setKommentar(scanner.next());
 
         index ++;
     }
