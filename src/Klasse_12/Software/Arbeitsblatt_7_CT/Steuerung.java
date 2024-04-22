e880732a8bfe8cf4daa5a649bd1ab2d26748eeb8
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2024-04-22 12:00:26 +0200

    Update Steuerung.java

--- src/Klasse_12/Software/Arbeitsblatt_7_CT/Steuerung.java
+++ src/Klasse_12/Software/Arbeitsblatt_7_CT/Steuerung.java
@@ -4,17 +4,18 @@ public class Steuerung {
     private static int aAnzahlHotels = 0;
     private GUI dieGUI;
     private Hotel dasHotel[];
+    private int aktHotel = 0;
     
     public Steuerung(GUI pGUI) {
     	dieGUI = pGUI;
     }
 	
-	private static double berechneSteuer(Hotel betrag[]) {
+	public double berechneSteuer(Hotel betrag[]) {
 		double vermoegen = berechneVermoegen(betrag);
 		return vermoegen * 0.08;
 	}
 	
-	private static double berechneVermoegen(Hotel betrag[]) {
+	public double berechneVermoegen(Hotel betrag[]) {
         double vermoegen = 0;
 		for(Hotel x:betrag) {
             if (x != null){
@@ -28,6 +29,16 @@ public class Steuerung {
 		aAnzahlHotels = anzHotel;
 		Japan.setKurs(kursYen);
 		Europa.setKurs(kursEuro);
+		dasHotel = new Hotel[aAnzahlHotels];
+	}
+	
+	public Hotel[] getHotels() {
+		return dasHotel;
+	}
+	
+	public void hotelErzeugen(Hotel hotel) {
+		dasHotel[aktHotel] = hotel;
+		aktHotel++;
 	}
 
 }
