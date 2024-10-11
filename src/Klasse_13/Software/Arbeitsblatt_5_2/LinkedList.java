a936aa0f0c3adfccf930f3d8f6e0e62710f7f504
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2024-10-11 07:47:29 +0000

    Linked List

--- src/Klasse_13/Software/Arbeitsblatt_5_2/LinkedList.java
+++ src/Klasse_13/Software/Arbeitsblatt_5_2/LinkedList.java
@@ -12,14 +12,122 @@ public class LinkedList<T>{
     }
 
     public void addFirst(T value){
-        this.firstElement = new Element<T>(value, this.firstElement);
-    }
-
-    public void addLast(T value){
-        
+        Element<T> newElement = new Element<T>(value, this.firstElement);
+        this.firstElement = newElement;
+        size++;
+        if (empty) empty = false;
     }
 
     public void add(T value){
-        
+        if(empty){
+            this.firstElement = new Element<T>(value);
+            empty = false;
+        }else{
+            Element<T> curElement = this.firstElement;
+
+            while(curElement.getNextElement() != null){
+                curElement = curElement.getNextElement();
+            }
+
+            curElement.setNextElement(new Element<T>(value));
+        }
+
+        size++;
     }
+
+    public void add(T value, int pos){
+        Element<T> curElement = this.firstElement;
+
+        for(int i = 0; i < pos - 1; i++){
+            curElement = curElement.getNextElement();
+        }
+
+        curElement.setNextElement(new Element<T>(value, curElement.getNextElement()));
+        size++;
+        if (empty) empty = false;
+    }
+
+    public Element<T> removeFirst(){
+        Element<T> removedElement = this.firstElement;
+
+        if (size == 1){
+            this.firstElement = null;
+            size = 0;
+            empty = true;
+            return removedElement;
+        }
+
+        this.firstElement = this.firstElement.getNextElement();
+        size--;
+        return removedElement;
+    }
+
+    public Element<T> removeLast(){
+        Element<T> removedElement;
+
+        if (size == 1){
+            removedElement = this.firstElement;
+            this.firstElement = null;
+            size = 0;
+            empty = true;
+            return removedElement;
+        }
+
+        Element<T> curElement = this.firstElement;
+
+        for(int i = 0; i < size - 2; i++){
+            curElement = curElement.getNextElement();
+        }
+
+        removedElement = curElement.getNextElement();
+
+        curElement.setNextElement(null);
+        size--;
+
+        return removedElement;
+    }
+
+    public Element<T> remove(int pos){
+        Element<T> removedElement;
+
+        if (pos == 0){
+            return removeFirst();
+        }
+
+        Element<T> curElement = this.firstElement;
+
+        for(int i = 0; i < pos - 1; i++){
+            curElement = curElement.getNextElement();
+        }
+
+        removedElement = curElement.getNextElement();
+
+        curElement.setNextElement(curElement.getNextElement().getNextElement());
+        size--;
+        if (size == 0) empty = true;
+        
+        return removedElement;
+    }
+
+    public void print(){
+        Element<T> curElement = this.firstElement;
+
+        System.out.print("[");
+
+        while (curElement != null){
+            System.out.print(curElement.getValue() + (curElement.getNextElement() != null ? ", " : ""));
+            curElement = curElement.getNextElement();
+        }
+
+        System.out.println("]");
+    }
+
+    public Boolean isEmpty(){
+        return empty;
+    }
+
+    public int length(){
+        return this.size;
+    }
+
 }
--- src/Klasse_13/Software/Arbeitsblatt_5_2/Steuerung.java
+++ src/Klasse_13/Software/Arbeitsblatt_5_2/Steuerung.java
@@ -2,6 +2,14 @@ package Arbeitsblatt_5_2;
 
 public class Steuerung {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        list.print();
+
+        list.add(6);
+        list.add(10);
+        list.print();
+
+        list.remove(1);
+        list.print();
     }
 }
